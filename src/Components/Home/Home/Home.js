58f52ee1fafe4ed53f4ebb961de286f98bb7384c
unknown <ronytalukder0014@gmail.com>
2023-01-04 16:30:11 +0600

    first commit

---
+++ src/Components/Home/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import useTitle from '../../../Hooks/useTitle';
+import Exercise from '../../../Components/Home/Exercise/Exercise'
+
+
+const Home = () => {
+    useTitle("Home");
+    return (
+        <div>
+            <Exercise></Exercise>
+        </div>
+    );
+};
+
+export default Home;
